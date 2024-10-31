e73d7d7dc44b8fb5156c768826718cbbdb272b01
K1ngHandy <bolds-slicks0g@icloud.com>
2024-10-30 21:34:33 -0400

    add dropdown arrow with slide in animation

--- About Me.swiftpm/App/HomeView.swift
+++ About Me.swiftpm/App/HomeView.swift
@@ -5,30 +5,50 @@ See the License.txt file for this sample’s licensing information.
 import SwiftUI
 
 struct HomeView: View {
+	@State private var isExpanded = false
+	
     var body: some View {
-        VStack {
-            Text(information.name)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding()
+		VStack(alignment: .leading) {
+			HStack {
+				Text(information.name)
+					.font(.largeTitle)
+					.fontWeight(.bold)
+					.padding()
+				Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
+					.rotationEffect(.degrees(isExpanded ? 100 : 0))
+					.animation(.easeInOut, value: isExpanded)
+			}
+			.onTapGesture {
+				withAnimation {
+					isExpanded.toggle()
+				}
+			}
 
-            Image(information.image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(50)
-                .padding(36)
+			if isExpanded {
+				VStack(alignment: .leading) {
+					Image(information.image)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.cornerRadius(50)
+						.padding(36)
+					
+					Text(information.story)
+						.font(.subheadline)
+						.padding()
+						.cornerRadius(20)
+						.overlay(
+							RoundedRectangle(cornerRadius: 20)
+								.stroke(Color.purple.opacity(0.9), lineWidth: 1.2)
+						)
+						.padding()
+				}
+				.transition(.slide)
+			}
 
-            Text(information.story)
-                .font(.subheadline)
-                .padding()
-                .cornerRadius(20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color.purple.opacity(0.9), lineWidth: 1.2)
-                )
+			Spacer()
         }
+		.padding()
     }
-    
 }
 
 struct HomeView_Previews: PreviewProvider {
