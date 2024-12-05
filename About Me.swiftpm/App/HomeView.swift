232dd95a020d11689b11e20ea26448e414737cb6
K1ngHandy <bolds-slicks0g@icloud.com>
2024-12-04 22:23:46 -0500

    add subtitle and ease out animation

--- About Me.swiftpm/App/HomeView.swift
+++ About Me.swiftpm/App/HomeView.swift
@@ -9,19 +9,27 @@ struct HomeView: View {
 	
     var body: some View {
 		VStack(alignment: .leading) {
-			HStack {
-				Text(information.name)
-					.font(.largeTitle)
-					.fontWeight(.bold)
-					.padding()
-				Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
-					.rotationEffect(.degrees(isExpanded ? 100 : 0))
-					.animation(.easeInOut, value: isExpanded)
-			}
-			.onTapGesture {
-				withAnimation {
-					isExpanded.toggle()
+			VStack {
+				HStack {
+					Text(information.name)
+						.font(.largeTitle)
+						.fontWeight(.bold)
+						.padding()
+					Image(systemName: isExpanded ? "chevron.down" : "chevron.up")
+						.rotationEffect(.degrees(isExpanded ? 0 : 100))
+						.animation(.easeInOut, value: isExpanded)
+					
+						.onTapGesture {
+							withAnimation {
+								isExpanded.toggle()
+							}
+						}
 				}
+				Text(isExpanded ? "" : "Expand for more...")
+					.padding(.leading)
+					.font(.title2)
+					.opacity(0.63)
+					.animation(.easeOut, value: isExpanded)
 			}
 
 			if isExpanded {
